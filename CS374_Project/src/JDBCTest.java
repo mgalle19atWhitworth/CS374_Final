980465e0b566f870ebc529264d32fe06227134d8
McKenna M Galle <mgalle19@my.whitworth.edu>
2017-11-16 10:52:05 -0800

    Added the java code

---
+++ CS374_Project/src/JDBCTest.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class JDBCTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try
+		{
+			double maxPrice = 20.0;
+			if (args.length > 0)
+				maxPrice = Double.parseDouble(args[0]);
+			
+			String stSQL = "SELECT C.Company, E.[Last Name] "
+                + "FROM Customers C, Employees E, Orders O "
+                + "WHERE E.ID=O.[Employee ID] AND C.ID=O.[Customer ID] AND "
+                + "O.[Shipping Fee] < " + String.valueOf(maxPrice)
+                + " ORDER BY O.[Shipping Fee]";
+
+Connection conn = DriverManager.getConnection(
+     "jdbc:odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};" 
+	      + "DBQ=C:\\Users\\mgalle19\\Documents\\GitHub\\CS374_Final\\Final_Project_CS374.accdb;UID=Admin;PWD=;");
+			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			stmt.execute(stSQL);
+
+			ResultSet rs = stmt.getResultSet();
+			while (rs.next() == true) {
+				System.out.println("<" + rs.getString(1) + ", " + 	rs.getString(2) + ">");
+			}
+		} 
+		catch (Exception ex) {
+			System.out.println(String.format("Error: %s", ex.getMessage()));
+		}
+	}
+
+}
